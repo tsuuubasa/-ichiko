7ea154f857f260e3dfcd832c7611230cc6b96a18
tsuuubasa <68677640+tsuuubasa@users.noreply.github.com>
2020-08-25 17:18:51 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+$(function(){
+
+$(window).scroll(function (){
+    $('.fadein').each(function(){
+        var elemPos = $(this).offset().top,
+            scroll = $(window).scrollTop(),
+            windowHeight = $(window).height();
+          if (scroll > elemPos - windowHeight + 100){
+              $(this).addClass('scrollin');
+            }
+        });
+    });
+
+});
